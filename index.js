c048f494c1293bfe849f5737d1d4300da68dbf70
rexim <reximkut@gmail.com>
2021-06-16 02:16:36 +0700

    Add support for custom titles

--- index.js
+++ index.js
@@ -1,27 +1,4 @@
 "use strict";
-function WireSelector(comps, callback) {
-    comps.thumb.onload = function () {
-        callback();
-    };
-    comps.link.onchange = function () {
-        try {
-            comps.error.innerText = '';
-            comps.thumb.src = '';
-            var url = new URL(comps.link.value);
-            var ytHostRegexp = new RegExp('^(.+\.)?youtube\.com$');
-            if (ytHostRegexp.test(url.hostname)) {
-                var ytVideoId = url.searchParams.getAll('v').join('');
-                comps.thumb.src = "http://i3.ytimg.com/vi/" + ytVideoId + "/maxresdefault.jpg";
-            }
-            else {
-                throw new Error('Only YouTube Links are supported');
-            }
-        }
-        catch (e) {
-            comps.error.innerText = e.message;
-        }
-    };
-}
 function getElementByIdOrDie(elementId) {
     var element = document.getElementById(elementId);
     if (element === null) {
@@ -29,34 +6,51 @@ function getElementByIdOrDie(elementId) {
     }
     return element;
 }
+function renderThumbnail(ctx, ytThumb, ytTitle) {
+    var w = 800.0;
+    var r = ytThumb.height / ytThumb.width;
+    var h = r * w;
+    var pad = 50;
+    ctx.canvas.width = w;
+    ctx.canvas.height = h;
+    ctx.drawImage(ytThumb, 0, 0, w, h);
+    var gradient = ctx.createLinearGradient(w * 0.5, h, w * 0.5, 0);
+    gradient.addColorStop(0.0, 'black');
+    gradient.addColorStop(1.0, '#00000000');
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, w, h);
+    ctx.font = '44px serif';
+    ctx.fillStyle = 'white';
+    ctx.fillText(ytTitle.value, pad, h - pad);
+}
 window.onload = function () {
     var ytLink = getElementByIdOrDie("yt-link");
     var ytError = getElementByIdOrDie("yt-error");
     var ytThumb = getElementByIdOrDie("yt-thumb");
     var ytCanvas = getElementByIdOrDie("yt-canvas");
+    var ytTitle = getElementByIdOrDie("yt-title");
     var ctx = ytCanvas.getContext('2d');
     if (ctx === null) {
         throw new Error("Could not initialize 2d context");
     }
-    WireSelector({
-        link: ytLink,
-        thumb: ytThumb,
-        error: ytError,
-    }, function () {
-        var w = 800.0;
-        var r = ytThumb.height / ytThumb.width;
-        var h = r * w;
-        var pad = 50;
-        ytCanvas.width = w;
-        ytCanvas.height = h;
-        ctx.drawImage(ytThumb, 0, 0, w, h);
-        var gradient = ctx.createLinearGradient(w * 0.5, h, w * 0.5, 0);
-        gradient.addColorStop(0.0, 'black');
-        gradient.addColorStop(1.0, '#00000000');
-        ctx.fillStyle = gradient;
-        ctx.fillRect(0, 0, w, h);
-        ctx.font = '48px serif';
-        ctx.fillStyle = 'white';
-        ctx.fillText('Hello, World', pad, h - pad);
-    });
+    ytTitle.onchange = function () { return renderThumbnail(ctx, ytThumb, ytTitle); };
+    ytThumb.onload = function () { return renderThumbnail(ctx, ytThumb, ytTitle); };
+    ytLink.onchange = function () {
+        try {
+            ytError.innerText = '';
+            ytThumb.src = '';
+            var url = new URL(ytLink.value);
+            var ytHostRegexp = new RegExp('^(.+\.)?youtube\.com$');
+            if (ytHostRegexp.test(url.hostname)) {
+                var ytVideoId = url.searchParams.getAll('v').join('');
+                ytThumb.src = "http://i3.ytimg.com/vi/" + ytVideoId + "/maxresdefault.jpg";
+            }
+            else {
+                throw new Error('Only YouTube Links are supported');
+            }
+        }
+        catch (e) {
+            ytError.innerText = e.message;
+        }
+    };
 };
--- index.ts
+++ index.ts
@@ -1,32 +1,3 @@
-interface WireSelectorComps {
-    thumb: HTMLImageElement,
-    link: HTMLInputElement,
-    error: HTMLElement,
-}
-
-function WireSelector(comps: WireSelectorComps, callback: () => void) {
-    comps.thumb.onload = function() {
-        callback();
-    };
-
-    comps.link.onchange = function() {
-        try {
-            comps.error.innerText = '';
-            comps.thumb.src = '';
-            const url = new URL(comps.link.value);
-            const ytHostRegexp = new RegExp('^(.+\.)?youtube\.com$');
-            if (ytHostRegexp.test(url.hostname)) {
-                const ytVideoId = url.searchParams.getAll('v').join('')
-                comps.thumb.src = `http://i3.ytimg.com/vi/${ytVideoId}/maxresdefault.jpg`;
-            } else {
-                throw new Error('Only YouTube Links are supported');
-            }
-        } catch(e) {
-            comps.error.innerText = e.message;
-        }
-    };
-}
-
 function getElementByIdOrDie(elementId: string): HTMLElement {
     const element = document.getElementById(elementId);
     if (element === null) {
@@ -35,39 +6,56 @@ function getElementByIdOrDie(elementId: string): HTMLElement {
     return element;
 }
 
+function renderThumbnail(ctx: CanvasRenderingContext2D, ytThumb: HTMLImageElement, ytTitle: HTMLInputElement): void {
+    const w = 800.0;
+    const r = ytThumb.height / ytThumb.width;
+    const h = r * w;
+    const pad = 50;
+
+    ctx.canvas.width  = w;
+    ctx.canvas.height = h;
+
+    ctx.drawImage(ytThumb, 0, 0, w, h);
+    let gradient = ctx.createLinearGradient(
+        w * 0.5, h,
+        w * 0.5, 0);
+    gradient.addColorStop(0.0, 'black');
+    gradient.addColorStop(1.0, '#00000000');
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, w, h);
+    // TODO: let the user control the font size
+    ctx.font = '44px serif';
+    ctx.fillStyle = 'white';
+    ctx.fillText(ytTitle.value, pad, h - pad);
+}
+
 window.onload = () => {
     const ytLink = getElementByIdOrDie("yt-link") as HTMLInputElement;
     const ytError = getElementByIdOrDie("yt-error") as HTMLElement;
     const ytThumb = getElementByIdOrDie("yt-thumb") as HTMLImageElement;
     const ytCanvas = getElementByIdOrDie("yt-canvas") as HTMLCanvasElement;
+    const ytTitle = getElementByIdOrDie("yt-title") as HTMLInputElement;
     const ctx = ytCanvas.getContext('2d');
     if (ctx === null) {
         throw new Error(`Could not initialize 2d context`);
     }
 
-    WireSelector({
-        link: ytLink,
-        thumb: ytThumb,
-        error: ytError,
-    }, () => {
-        const w = 800.0;
-        const r = ytThumb.height / ytThumb.width;
-        const h = r * w;
-        const pad = 50;
-
-        ytCanvas.width  = w;
-        ytCanvas.height = h;
-
-        ctx.drawImage(ytThumb, 0, 0, w, h);
-        let gradient = ctx.createLinearGradient(
-            w * 0.5, h,
-            w * 0.5, 0);
-        gradient.addColorStop(0.0, 'black');
-        gradient.addColorStop(1.0, '#00000000');
-        ctx.fillStyle = gradient;
-        ctx.fillRect(0, 0, w, h);
-        ctx.font = '48px serif';
-        ctx.fillStyle = 'white';
-        ctx.fillText('Hello, World', pad, h - pad);
-    });
+    ytTitle.onchange = () => renderThumbnail(ctx, ytThumb, ytTitle);
+    ytThumb.onload = () => renderThumbnail(ctx, ytThumb, ytTitle);
+    ytLink.onchange = () => {
+        try {
+            ytError.innerText = '';
+            ytThumb.src = '';
+            const url = new URL(ytLink.value);
+            const ytHostRegexp = new RegExp('^(.+\.)?youtube\.com$');
+            if (ytHostRegexp.test(url.hostname)) {
+                const ytVideoId = url.searchParams.getAll('v').join('')
+                ytThumb.src = `http://i3.ytimg.com/vi/${ytVideoId}/maxresdefault.jpg`;
+            } else {
+                throw new Error('Only YouTube Links are supported');
+            }
+        } catch(e) {
+            ytError.innerText = e.message;
+        }
+    };
 };
